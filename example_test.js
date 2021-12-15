1d2acda766737ff439427eee8fc6cf7464eadb2b
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-15 17:02:48 -0500

    Add multiple scenarios

--- example_test.js
+++ example_test.js
@@ -1,10 +1,15 @@
 Feature('example');
 
-Scenario('test something', ({ I }) => {
+Scenario('Test 1', ({ I }) => {
 	I.amOnPage('http://www.example.com');
 	I.wait(1);
 	I.waitForElement('h1');
 	I.see('Example');
+});
+
+Scenario('Test 2', ({ I }) => {
+	I.amOnPage('http://www.example.com');
+	I.wait(1);
 	I.dontSeeElement('.login-form');
 	I.seeInCurrentUrl('example.com');
 });
