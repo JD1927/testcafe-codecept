9596e4b25029bf627b4ada55178e4c09f6218bb8
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-15 16:36:10 -0500

    Add codecept assertions

--- example_test.js
+++ example_test.js
@@ -4,4 +4,7 @@ Scenario('test something', ({ I }) => {
 	I.amOnPage('http://www.example.com');
 	I.wait(1);
 	I.waitForElement('h1');
+	I.see('Example');
+	I.dontSeeElement('.login-form');
+	I.seeInCurrentUrl('example.com');
 });
