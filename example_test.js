8f9ad9843e9acaf3dd907dd374d1bc6caa221d2c
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-21 20:02:55 -0500

    Working with forms

--- example_test.js
+++ example_test.js
@@ -1,26 +1,10 @@
 Feature('example');
 
-Scenario('Test 1', ({ I }) => {
-	I.amOnPage('http://www.example.com');
-	I.wait(1);
-	I.waitForElement('h1');
-	I.see('Example');
-});
-
-Scenario('Test 2', ({ I }) => {
-	I.amOnPage('http://www.example.com');
-	I.wait(1);
-	I.dontSeeElement('.login-form');
-	I.seeInCurrentUrl('example.com');
-});
-
-Scenario('Full page screenshot', ({ I }) => {
-	I.amOnPage('http://www.example.com');
-	I.wait(1);
-	I.saveScreenshot('test.png', true);
-});
-Scenario('Element page screenshot', ({ I }) => {
-	I.amOnPage('http://www.example.com');
-	I.wait(1);
-	I.saveElementScreenshot('h1', 'element.png');
+Scenario('Login Test', ({ I }) => {
+	I.amOnPage('http://zero.webappsecurity.com/login.html');
+	I.waitForElement('#login_form');
+	I.fillField('#user_login', 'invalid-username');
+	I.fillField('#user_password', 'invalid-password');
+	I.click('input[type="submit"]');
+	I.waitForText('Login and/or password are wrong.');
 });
