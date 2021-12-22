c23ff80d54e0ff2106eb235a6d52760bfa29eee1
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-21 20:10:58 -0500

    Grabbing text and value

--- example_test.js
+++ example_test.js
@@ -1,10 +1,18 @@
 Feature('example');
 
-Scenario('Login Test', ({ I }) => {
-	I.amOnPage('http://zero.webappsecurity.com/login.html');
-	I.waitForElement('#login_form');
-	I.fillField('#user_login', 'invalid-username');
-	I.fillField('#user_password', 'invalid-password');
-	I.click('input[type="submit"]');
-	I.waitForText('Login and/or password are wrong.');
+// Scenario('Login Test', ({ I }) => {
+// 	I.amOnPage('http://zero.webappsecurity.com/login.html');
+// 	I.waitForElement('#login_form');
+// 	I.fillField('#user_login', 'invalid-username');
+// 	I.fillField('#user_password', 'invalid-password');
+// 	I.click('input[type="submit"]');
+// 	I.waitForText('Login and/or password are wrong.');
+// });
+
+Scenario('Extract Test', async ({ I }) => {
+	I.amOnPage('http://www.example.com/');
+	let text = await I.grabTextFrom('h1');
+	console.log(`Text: ${text}`);
+	let value = await I.grabValueFrom('h1');
+	console.log(`Value: ${value}`);
 });
