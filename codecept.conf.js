9b1b4e17fbbf663747bb93c2405752416fcb8a03
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2021-12-21 20:21:46 -0500

    Add advance configurations

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,31 +5,34 @@ const { setHeadlessWhen } = require('@codeceptjs/configure');
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
-  helpers: {
-    TestCafe: {
-      url: 'http://localhost',
-      browser: 'chrome',
-      show: false
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'testcafe-codecept',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: false
-    },
-    tryTo: {
-      enabled: false
-    },
-    screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
+	tests: './*_test.js',
+	output: './output',
+	helpers: {
+		TestCafe: {
+			url: 'http://localhost',
+			browser: 'chrome',
+			show: true,
+			waitForTimeout: 15000,
+			keepCookies: true,
+			windowSize: '800x600',
+		},
+	},
+	include: {
+		I: './steps_file.js',
+	},
+	bootstrap: null,
+	mocha: {},
+	name: 'testcafe-codecept',
+	plugins: {
+		pauseOnFail: {},
+		retryFailedStep: {
+			enabled: false,
+		},
+		tryTo: {
+			enabled: false,
+		},
+		screenshotOnFail: {
+			enabled: true,
+		},
+	},
+};
